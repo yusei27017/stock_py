c21cd014769d875283cc33974ae1b7e7a33be1b0
yusei27017 <no101shark@gmail.com>
2024-02-26 22:16:46 +0800

    crawl stock price

--- python_dev/twes_data_get.py
+++ python_dev/twes_data_get.py
@@ -1,16 +1,33 @@
 import pandas as pd
 import requests
 import json
+from datetime import date
 
-# print("hello world")
-if __name__ == "__main__":
-    print("start")
-    sess = requests.session()
-    res = sess.get("https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date=20240223&type=ALL&response=json")
+sess = requests.session()
+
+def crawl_twse_data(crawl_date):
+    twse_url = f"https://www.twse.com.tw/rwd/zh/afterTrading/MI_INDEX?date={crawl_date}&type=ALL&response=json"
+    res = sess.get(twse_url)
     res_json = json.loads(res.text)
     data_tbl = res_json['tables']
     tbl_all_data = data_tbl[8]['data']
     df = pd.DataFrame(tbl_all_data)
     df.astype(str)
-    print(df)
-    # print(tbl_all_data[:100])
+    df = df.apply(lambda s: s.str.replace(',', ''))
+    df['date'] = pd.to_datetime(crawl_date)
+    # 將「證券代號」的欄位改名成「stock_id」
+    df = df.rename(columns={0:'stock_id'})
+    # 將 「stock_id」與「date」設定成index 
+    df = df.set_index(['stock_id', 'date'])
+    # 將所有的表格元素都轉換成數字，error='coerce'的意思是說，假如無法轉成數字，則用 NaN 取代
+    df = df.apply(lambda s:pd.to_numeric(s, errors='coerce'))    
+    # 刪除不必要的欄位
+    df = df[df.columns[df.isnull().all() == False]]
+    return df
+
+if __name__ == "__main__":
+    print("start")
+    # today = date.today()
+    # formatted_date = today.strftime('%Y%m%d')
+    df = crawl_twse_data("20240223")
+    print(df.head())
